6a432e4f2c2c5853f7530e962930e0f7dc05602b
charlie_kickflip <c.p0yne@gmail.com>
2022-12-01 16:41:51 -0500

    added df header info in feature_gen.py

--- epg/feature_gen.py
+++ epg/feature_gen.py
@@ -15,9 +15,8 @@
 #  [none]
 
 # external imports
-# import csv
+import csv
 import pandas as pd
-# import matplotlib.pyplot as plt
 # from tsfresh import extract_features
 # from tsfresh.utilities.dataframe_functions import impute
 
@@ -26,13 +25,26 @@ from config import user_config as cfg
 
 
 if __name__ == '__main__':
+    # load in the data
     print("- loading in the data")
     df_patients = {}  # a dictionary of data frames per patient
-    for ii in cfg.patients:
-        print(f"  -- loading in for patient {ii}...")
-        df_patients[ii] = pd.read_csv(cfg.archive + '/' + str(ii) + '.csv/' + str(ii) + '.csv', header=None)
+    for pp in cfg.patients:
+        print(f"  -- loading in for patient {pp}...")
+        df_patients[pp] = pd.read_csv(cfg.archive + '/' + str(pp) + '.csv/' + str(pp) + '.csv', header=None)
         print("     ...done.")
     
-    for key in df_patients:
-        print(key)
-        print(df_patients[key])
+    # grab and distribute the headers
+    with open(cfg.archive + '/columnLabels.csv', 'r') as fin:
+        heading = list(csv.reader(fin))[0]
+
+    print("\n- setting the header and etc")
+    print(f"  -- heading = {heading}\n")
+    
+    for pp in df_patients:
+        df_patients[pp].columns = heading
+        df_patients[pp]['subject'] = df_patients[pp]['subject'].astype('int')
+        df_patients[pp]['trial'] = df_patients[pp]['trial'].astype('int')
+        df_patients[pp]['condition'] = df_patients[pp]['condition'].astype('int')
+        df_patients[pp]['sample'] = df_patients[pp]['sample'].astype('int')
+        print(f"  -- patient {pp}, df =")
+        print(df_patients[pp], '\n')
