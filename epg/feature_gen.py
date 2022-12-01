882ee0d08aa98b0cb15a84c7e8e026e609eeb1e8
charlie_kickflip <c.p0yne@gmail.com>
2022-12-01 18:40:32 -0500

    put the waves in series for feature_gen.py

--- epg/feature_gen.py
+++ epg/feature_gen.py
@@ -12,7 +12,8 @@
 # KNOWN BUGS
 #  [none]
 # DESIRED FEATURES
-#  [none]
+#  -- add a time stamp
+#  -- make the patient data frames into a class?
 
 # external imports
 import csv
@@ -24,6 +25,20 @@ import pandas as pd
 from config import user_config as cfg
 
 
+# FUNCTION: print_first_three = print the first three patient dataframes
+def print_first_three(df_patients):
+    for pp in list(df_patients.keys())[:3]:
+        print(f"~~~~ patient {pp}, df =")
+        print(df_patients[pp], '\n')
+
+
+# FUNCTION: tss = put the time series in series wrt trial
+#   IN/OUT: row = a row of the patient data frame
+def tss(row):
+    row['sample'] += (row['trial']-1)*cfg.sample_count
+    return row
+
+
 if __name__ == '__main__':
     # load in the data
     print("- loading in the data")
@@ -33,10 +48,10 @@ if __name__ == '__main__':
         df_patients[pp] = pd.read_csv(cfg.archive + '/' + str(pp) + '.csv/' + str(pp) + '.csv', header=None)
         print("     ...done.")
     
-    # grab and distribute the headers
+    # grab and distribute the headers, separate out the selected_condition
     with open(cfg.archive + '/columnLabels.csv', 'r') as fin:
         heading = list(csv.reader(fin))[0]
-
+    
     print("\n- setting the header and etc")
     print(f"  -- heading = {heading}\n")
     
@@ -46,7 +61,23 @@ if __name__ == '__main__':
         df_patients[pp]['trial'] = df_patients[pp]['trial'].astype('int')
         df_patients[pp]['condition'] = df_patients[pp]['condition'].astype('int')
         df_patients[pp]['sample'] = df_patients[pp]['sample'].astype('int')
-        print(f"  -- patient {pp}, df =")
-        print(df_patients[pp], '\n')
+        
+        df_patients[pp] = df_patients[pp][df_patients[pp]['condition'] == cfg.selected_condition]
+    
+    print_first_three(df_patients)
+    
+    # put the waves in series
+    print("- line up the trials into a series")
+    for pp in df_patients:
+        print(f"  -- lining up for patient {pp}...")
+        df_patients[pp][['sample', 'trial']] = \
+            df_patients[pp][['sample', 'trial']].apply(lambda row: tss(row), axis=1)
+        
+        df_patients[pp] = df_patients[pp].sort_values(by=['sample'])
+        df_patients[pp].reset_index(inplace=True, drop=True)
+        print("     ...done.")
+    
+    print_first_three(df_patients)
+
 
 # F- I-- N---
