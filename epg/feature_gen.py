9ccfdaf28ccc5b28473ad4523f6ac976d1cd7314
charlie_kickflip <c.p0yne@gmail.com>
2022-12-01 22:27:22 -0500

    rewrote feature_gen.py to utilize the new PatientDF class

--- epg/feature_gen.py
+++ epg/feature_gen.py
@@ -25,11 +25,41 @@ import pandas as pd
 from config import user_config as cfg
 
 
+# CLASS: PatientDF = a pandas data frame with properties for a patient sample
+class PatientDF():
+    # CONSTRUCTOR
+    def __init__(self, df):
+        self.df = df
+    
+    # METHOD: tidy_up = add a header and reassign data types in the df
+    def tidy_up(self, header):
+        self.df.columns = header
+        self.df['subject'] = self.df['subject'].astype('int')
+        self.df['trial'] = self.df['trial'].astype('int')
+        self.df['condition'] = self.df['condition'].astype('int')
+        self.df['sample'] = self.df['sample'].astype('int')
+    
+    # METHOD: set_condition = pull out a specific condition from the df
+    #     IN: selected_condition = the condition number to pull out, set in user_config.py
+    def set_condition(self, selected_condition):
+        self.df = self.df[self.df['condition'] == selected_condition]
+    
+    # METHOD: line_up_time_series = concatenate the time series in series wrt trial
+    def line_up_time_series(self):
+        # concatenate all the time series together
+        self.df[['sample', 'trial']] = \
+            self.df[['sample', 'trial']].apply(lambda row: tss(row), axis=1)
+        
+        # sort by sample
+        self.df = self.df.sort_values(by=['sample'])
+        self.df.reset_index(inplace=True, drop=True)
+
+
 # FUNCTION: print_first_three = print the first three patient dataframes
-def print_first_three(df_patients):
-    for pp in list(df_patients.keys())[:3]:
+def print_first_three(pat_dat):
+    for pp in list(pat_dat.keys())[:3]:
         print(f"~~~~ patient {pp}, df =")
-        print(df_patients[pp], '\n')
+        print(pat_dat[pp].df, '\n')
 
 
 # FUNCTION: tss = put the time series in series wrt trial
@@ -41,43 +71,33 @@ def tss(row):
 
 if __name__ == '__main__':
     # load in the data
-    print("- loading in the data")
-    df_patients = {}  # a dictionary of data frames per patient
+    print("- load in the data")
+    pat_dat = {}  # a dictionary of PatientDF's per patient
     for pp in cfg.patients:
         print(f"  -- loading in for patient {pp}...")
-        df_patients[pp] = pd.read_csv(cfg.archive + '/' + str(pp) + '.csv/' + str(pp) + '.csv', header=None)
+        pat_dat[pp] = PatientDF(pd.read_csv(cfg.archive + '/' + str(pp) + '.csv/' + str(pp) + '.csv', header=None))
         print("     ...done.")
     
-    # grab and distribute the headers, separate out the selected_condition
+    # grab the header, tidy up, and separate out the chosen condition
     with open(cfg.archive + '/columnLabels.csv', 'r') as fin:
-        heading = list(csv.reader(fin))[0]
+        header = list(csv.reader(fin))[0]
+    print(f"  -- header = {header}\n")
     
-    print("\n- setting the header and etc")
-    print(f"  -- heading = {heading}\n")
+    print("- restructure the data frames (tidy up + set condition)")
+    for pp in pat_dat:
+        pat_dat[pp].tidy_up(header)
+        pat_dat[pp].set_condition(cfg.selected_condition)
     
-    for pp in df_patients:
-        df_patients[pp].columns = heading
-        df_patients[pp]['subject'] = df_patients[pp]['subject'].astype('int')
-        df_patients[pp]['trial'] = df_patients[pp]['trial'].astype('int')
-        df_patients[pp]['condition'] = df_patients[pp]['condition'].astype('int')
-        df_patients[pp]['sample'] = df_patients[pp]['sample'].astype('int')
-        
-        df_patients[pp] = df_patients[pp][df_patients[pp]['condition'] == cfg.selected_condition]
-    
-    print_first_three(df_patients)
+    print_first_three(pat_dat)
     
     # put the waves in series
     print("- line up the trials into a series")
-    for pp in df_patients:
+    for pp in pat_dat:
         print(f"  -- lining up for patient {pp}...")
-        df_patients[pp][['sample', 'trial']] = \
-            df_patients[pp][['sample', 'trial']].apply(lambda row: tss(row), axis=1)
-        
-        df_patients[pp] = df_patients[pp].sort_values(by=['sample'])
-        df_patients[pp].reset_index(inplace=True, drop=True)
+        pat_dat[pp].line_up_time_series()
         print("     ...done.")
     
-    print_first_three(df_patients)
+    print_first_three(pat_dat)
 
 
 # F- I-- N---
