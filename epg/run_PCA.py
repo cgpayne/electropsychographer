58c2fc056b92bb86f786eb8f3bcfefdabd64f84f
charlie_kickflip <c.p0yne@gmail.com>
2023-01-13 22:34:11 -0500

    prototyped code to create response variable in run_PCA.py

--- epg/run_PCA.py
+++ epg/run_PCA.py
@@ -17,8 +17,29 @@
 # DESIRED FEATURES
 #  [none]
 
+# external imports
+# import numpy as np
+import pandas as pd
+
+import epglib.params as p_epg
+from config import user_config as cfg
+# import epglib.utils as ut
+# import epglib.classes as cls
+
 
 if __name__ == '__main__':
+    # create response variable
+    demo = pd.read_csv(cfg.meta_dir + '/' + p_epg.fdemographic)
+    print(demo)
+    
+    all_subjects = {demo.loc[ii, 'subject']: demo.loc[ii, ' group'] for ii in range(len(demo))}
+    print(all_subjects)
+    
+    # df.index.name = 'subject' (for feature_gen.py)
+    X = pd.DataFrame({'subject': [27,71,2,14,15,69,18,80], 'xyz': [88,89,90,91,92,9,93,94]})  # test
+    y = pd.DataFrame({'subject': list(X['subject']),
+                      'response': [all_subjects[X.loc[ii, 'subject']] for ii in range(len(X))]})
+    print(y)
     
     
     # F- I-- N---
